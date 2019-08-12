1ed6fa0f2e962b4c041fbacc2ca735c66c646e12
Dylan Sun <dylan_sun@intuit.com>
2019-08-12 16:29:26 -0700

    stuff

--- basic-func/math.go
+++ basic-func/math.go
@@ -5,10 +5,16 @@ import (
 )
 
 
-func Addition( num1 int32,  num2 int32) int32 {
+func Addition( num1 int32,  num2 int32)  {
 	fmt.Println("Adding....")
 	tot := num1 + num2
 	fmt.Print("Total: ")
 	fmt.Println(tot)
-	return num1 + num2
+}
+
+func Multiply( num1 int32,  num2 int32) {
+	fmt.Println("Multiplying....")
+	tot := num1 * num2
+	fmt.Print("Total: ")
+	fmt.Println(tot)
 }
